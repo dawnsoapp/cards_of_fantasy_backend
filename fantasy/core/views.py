884d234abd74d11c3309960192a433022d59b472
Madison Jackson <madi.jackson29@gmail.com>
2023-08-14 10:34:37 -0700

    created models and started tackling urls and views

--- fantasy/core/views.py
+++ fantasy/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from core.models import Question, Score
 
 # Create your views here.
+# get_all_questions(["GET"]):
+#     pass
