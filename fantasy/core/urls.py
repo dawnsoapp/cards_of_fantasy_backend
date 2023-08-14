c29140611f18a5b7682d65628a7b0a8ac57d4f49
Madison Jackson <madi.jackson29@gmail.com>
2023-08-14 13:46:31 -0700

    using local machine to test api requests

--- fantasy/core/urls.py
+++ fantasy/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.post_question, name="question"),
+    path("", views.read_questions, name="question"),
 ]
--- fantasy/core/views.py
+++ fantasy/core/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 from core.models import Question, Score
+from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
-# get_all_questions(["GET"]):
-#     pass
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the core index.")
+#CREATE
+def post_question(request):
+    if request.method == "POST":
+        question_object = Question.objects.all()
+        question = Question.to_dict(question_object)
+        return JsonResponse(question)
+
+#READ
+def read_questions(request):
+    if request.method == "GET":
+        questions = Question.objects.all()
+        question_list = [question.to_dict(questions) for question in questions]
+        return JsonResponse(question_list)
